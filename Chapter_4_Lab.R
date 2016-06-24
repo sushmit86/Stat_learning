a69708f4ab174182cc07935c6bd0238e122c027e
SUSHMIT ROY <sushmitroy@SUSHMITs-MacBook-Pro.local>
2016-06-23 23:33:35 -0400

    Adding

--- Chapter_4_Lab.R
+++ Chapter_4_Lab.R
@@ -1,4 +1,5 @@
 library(ISLR)
+library(MASS)
 names(Smarket)
 head(Smarket)
 dim(Smarket)
@@ -38,3 +39,21 @@ table(glm_pred, Direction_2005)
 mean(glm_pred == Direction_2005)
 mean(glm_pred != Direction_2005)
 # 106/(106+ 76)
+predict(glm_fit, newdata = data.frame(Lag1= c(1.2,1.5), Lag2 = c(1.1,-0.8)), type = "response")
+# Linear Discriminant analysis
+lda_fit = lda(Direction ~ Lag1 + Lag2 , data = Smarket, subset = train) 
+plot(lda_fit)
+lda_pred = predict(lda_fit, Smarket_2005)
+names(lda_pred)
+lda_class = lda_pred$class
+table(lda_class, Direction_2005)
+mean(lda_class == Direction_2005)
+lda_pred$class[1:10]
+lda_pred$posterior[1:10]
+lda_pred$x[1]
+head(Smarket_2005)
+sum(lda_pred$posterior[,1] >= 0.5)
+sum(lda_pred$posterior[,1] < 0.5)
+lda_pred$posterior[1:20,1]
+lda_class[1:20]
+## QDA
