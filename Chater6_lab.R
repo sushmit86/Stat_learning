ec6356763f652776ddf09630b57365bea4b55016
sushmit <sroy@nyc-007152-mbp.local>
2016-07-24 02:07:05 -0400

    Adding

--- Chater6_lab.R
+++ Chater6_lab.R
@@ -38,3 +38,22 @@ coef(regfit.full,7)
 coef(regfit.fwd,7)
 coef(regfit.bwd,7)
 # Choosing Among models using the validation set approach
+set.seed(1)
+train = sample(c(TRUE, FALSE), nrow(Hitters), rep = TRUE)
+test = (!train)
+regfit.best = regsubsets(Salary ~., data = Hitters[train,], nvmax = 19)
+test.mat = model.matrix(Salary~., data = Hitters[test, ])
+val.errors = rep(NA,19)
+for (i in 1:19){
+  coefi = coef(regfit.best, id =i)
+  pred = test.mat[, names(coefi)]%*%coefi
+  val.errors[i] = mean((Hitters$Salary[test] - pred)^2)
+}
+View(Hitters)
+predict.regsubsets = function(object, newdata, id,...){
+form = as.formula(object$call[[2]])
+mat = model.matrix(form, newdata)
+coefi = coef(object, id =id)
+xvars = names(coefi)
+mat[,xvars]%*%coefi
+}
